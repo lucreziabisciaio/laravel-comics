e4e3a33190a6a11120fde5af6b24574411f7ccea
lucreziabisciaio <lucreziabisciaio@gmail.com>
2022-03-03 20:07:11 +0100

    adds bootstrap js cdn + shop dropdown menu in navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -22,12 +22,26 @@ $navLinks = [
     <div class="nav_links">
         <ul class="d-flex mb-0">
             @foreach ($navLinks as $k => $navLink)
-                <li>
+                <li class="dynamic">
                     <a href="{{ route($k) }}">
                         {{ $navLink }}
                     </a>
                 </li>
             @endforeach
+
+            <li class="dynamic">
+                <div class="dropdown">
+                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                        shop
+                    </a>
+
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">Link #1</a></li>
+                        <li><a class="dropdown-item" href="#">Link #2</a></li>
+                        <li><a class="dropdown-item" href="#">Link #3</a></li>
+                    </ul>
+                </div>
+            </li>
         </ul>
     </div>
 </div>
